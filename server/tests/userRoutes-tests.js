fb66b8774096019a0326f32888ddce7d4424b4d0
sfncook <cooksha@yahoo-inc.com>
2019-06-19 19:54:00 -0700

    getUsers

--- server/tests/userRoutes-tests.js
+++ server/tests/userRoutes-tests.js
@@ -5,11 +5,11 @@ const expect = chai.expect;
 chai.use(sinonChai);
 const { mockReq, mockRes } = require('sinon-express-mock');
 
-const MockLogger = require('../mocks/mockLogger');
-const MockConfig = require('../mocks/mockConfig');
-const MockFileLoader = require('../mocks/mockFileLoader');
-const MockUserParser = require('../mocks/mockUserParser');
-const MockGroupParser = require('../mocks/mockGroupParser');
+const MockLogger = require('./mocks/mockLogger');
+const MockConfig = require('./mocks/mockConfig');
+const MockFileLoader = require('./mocks/mockFileLoader');
+const MockUserParser = require('./mocks/mockUserParser');
+const MockGroupParser = require('./mocks/mockGroupParser');
 const UserRoutes = require('../routes/userRoutes');
 
 // Mocks for injection
@@ -31,47 +31,43 @@ let defaultRequest = {
 // Object to test
 let userRoutes;
 
-const init = (method, path)=>{
-  return ()=>{
-    mockLogger = new MockLogger(sinon);
-    mockConfig = new MockConfig(sinon);
-    mockFileLoader = new MockFileLoader(sinon);
-    mockUserParser = new MockUserParser(sinon);
-    mockGroupParser = new MockGroupParser(sinon);
+const init = ()=>{
+  mockLogger = new MockLogger(sinon);
+  mockConfig = new MockConfig(sinon);
+  mockFileLoader = new MockFileLoader(sinon);
+  mockUserParser = new MockUserParser(sinon);
+  mockGroupParser = new MockGroupParser(sinon);
 
-    const nextCallback = ()=>{};
-    mockNext = sinon.spy(nextCallback);
+  const nextCallback = ()=>{};
+  mockNext = sinon.spy(nextCallback);
 
-    defaultRequest.method = method;
-    defaultRequest.originalUrl = path;
-
-    //Object to test
-    userRoutes = new UserRoutes(mockLogger, mockConfig, mockFileLoader, mockUserParser, mockGroupParser);
-  }
+  //Object to test
+  userRoutes = new UserRoutes(mockLogger, mockConfig, mockFileLoader, mockUserParser, mockGroupParser);
 };
 
 describe('UserRoutes Tests', function () {
-  describe('_preAuthMachineId', function () {
-    beforeEach(init());
+  describe('getUsers', function () {
+    beforeEach(init);
 
-    it(`(negative) selectWhere throws error`, async ()=>{
+    it('simple call succeeds', async ()=>{
       const req = mockReq(defaultRequest);
       const res = mockRes();
-      const expectedMachineId = 1234;
-      req.query.machine_id = expectedMachineId;
-      const mockUsers_userHasRoleInReq_stub = sinon.stub(mockUsers, 'userHasRoleInReq');
-      mockUsers_userHasRoleInReq_stub.withArgs(req, Roles.rootAdmin).returns(false);
-      mockUsers_userHasRoleInReq_stub.returns(false);
-      const expectedErrorThrown = {foo:'bar'};
-      const mockSql_selectWhere_stub = sinon.stub(mockSql, 'selectWhere');
-      mockSql_selectWhere_stub.throws(expectedErrorThrown);
-      sinon.spy(AbstractRoutes.prototype, 'handleError');
 
-      runtimeRoutes._preAuthMachineId(req, res, mockNext);
+      const expectedLoadFileLines = 'expectedLoadFileLines';
+      userRoutes.usersPath = 'userRoutes.usersPath';
+      userRoutes.groupsPath = 'userRoutes.groupsPath';
+      mockFileLoader.loadFile.restore();
+      sinon.stub(mockFileLoader, 'loadFile').returns(Promise.resolve(expectedLoadFileLines));
+      const expectedResponse = 'expectedResponse';
+      mockUserParser.parse.restore();
+      sinon.stub(mockUserParser, 'parse').returns(expectedResponse);
 
-      sinon.assert.calledOnce(AbstractRoutes.prototype.handleError);
-      sinon.assert.calledWithExactly(AbstractRoutes.prototype.handleError, expectedErrorThrown, req, res);
-      sinon.assert.calledWithExactly(mockSql_selectWhere_stub, SqlTableNames.machines, 'id', expectedMachineId, true);
+      await userRoutes.getUsers(req, res, mockNext);
+
+      sinon.assert.calledOnce(mockFileLoader.loadFile);
+      sinon.assert.calledWithExactly(mockFileLoader.loadFile, userRoutes.usersPath);
+      sinon.assert.calledOnce(mockUserParser.parse);
+      sinon.assert.calledWithExactly(mockUserParser.parse, expectedLoadFileLines);
       sinon.assert.notCalled(mockNext);
     });
 
